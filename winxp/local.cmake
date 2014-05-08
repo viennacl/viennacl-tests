138a4eef269c46e64f672df11a0003f7f6199923
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-05-08 19:25:21 +0200

    added boost directory

--- winxp/local.cmake
+++ winxp/local.cmake
@@ -1,4 +1,4 @@
 set(ROOT_DIRECTORY "D:/git/ViennaCL")
 set(CTEST_SOURCE_DIRECTORY $ENV{VIENNACLPATH})
 set(USED_PROJECTS "VIENNACL")
-set(CTEST_BUILD_OPTIONS "")
+set(CTEST_BUILD_OPTIONS "-DBOOST_ROOT=C:\\Dokumente\ und\ Einstellungen\\Joe\\Eigene\ Dateien\\Downloads\\boost_1_55_0\\boost_1_55_0")
