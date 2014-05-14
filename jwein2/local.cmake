1c71f88ccb758a57618df0a71e65ca1f935340dd
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-05-14 10:40:03 +0200

    removed jwein2 because of hardware issues

--- jwein2/local.cmake
+++
@@ -1,8 +0,0 @@
-set(ROOT_DIRECTORY "/home/vdev/git/ViennaCL")
-set(CTEST_SOURCE_DIRECTORY $ENV{VIENNACLPATH})
-set(USED_PROJECTS "VIENNACL")
-set(CTEST_BUILD_OPTIONS "")
-
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
-set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
